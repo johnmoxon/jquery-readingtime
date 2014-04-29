468c6c8bd6f38858d29050803a94ac23624b4dbc
John Moxon <jmoxon@gmail.com>
2014-04-29 21:29:07 +1000

    bumped version and cleaned up for tag

--- jquery.readingtime.js
+++ jquery.readingtime.js
@@ -9,10 +9,7 @@
     var words = ($.type(options) === 'number' ) ?
       options : $.trim(this.first().text()).split(/\s+/).length;
 
-    console.log(words, "words");
-
     var eta = Math[settings.round](words/settings.wpm);
-    console.log(eta, 'eta')
     return (eta < 1) ? 'Less than a minute' : eta + ' mins';
   };
 }(jQuery));
